e66a1d9675b7fbb3f1d7d45f9f3621fe887394fd
AndreaMoriconi <moriconiandrea98@gmail.com>
2022-11-21 22:14:57 +0100

    added primo accesso doc

--- js/myscript.js
+++ js/myscript.js
@@ -9,3 +9,30 @@ function myFunction(elementID) {
     }
 }
   
+function showBrowser(browser) {
+    
+    var chrome = document.getElementById('chrome');
+    var safari = document.getElementById('safari');
+    var edge = document.getElementById('edge');
+
+    switch (browser){
+
+        case "chrome":
+            chrome.style.display = "block";
+            safari.style.display = "none";
+            edge.style.display = "none";
+            break;
+        case "safari":
+            chrome.style.display = "none";
+            safari.style.display = "block";
+            edge.style.display = "none";
+            break;
+        case "edge": 
+            chrome.style.display = "none";
+            safari.style.display = "none";
+            edge.style.display = "block";
+            break;
+    }
+   
+    
+}
